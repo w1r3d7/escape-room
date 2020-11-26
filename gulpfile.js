3cd5bd08c024fd049bdbffb756b128107d052293
Sergey Klimov <desire0417@gmail.com>
2020-11-26 16:16:27 +0300

    добавляет вертку шапки главной

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('server', function () {
     ui: false
   });
 
-  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
+  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css', 'refresh'));
   gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
   gulp.watch('source/*.html', gulp.series('html', 'refresh'));
   gulp.watch('source/js/*.js', gulp.series('html', 'build'));

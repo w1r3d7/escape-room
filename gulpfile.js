0b93f734588400e2d49aab26337e4495018f59f3
Sergey Klimov <desire0417@gmail.com>
2020-12-09 17:40:47 +0300

    исправляет js

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,6 @@ var svgstore = require('gulp-svgstore');
 var posthtml = require('gulp-posthtml');
 var include = require('posthtml-include');
 var del = require('del');
-var concat = require('gulp-concat');
 
 gulp.task('css', function () {
   return gulp.src('source/sass/style.scss')
@@ -88,7 +87,7 @@ gulp.task('copy', function () {
   return gulp.src([
     'source/fonts/**/*.{woff,woff2}',
     'source/img/**',
-    'source/js/vendor.js',
+    'source/js/**.js',
     'source//*.ico'
   ], {
     base: 'source'
@@ -96,18 +95,9 @@ gulp.task('copy', function () {
       .pipe(gulp.dest('build'));
 });
 
-gulp.task('scripts', function () {
-  return gulp.src([
-    'source/js/*.js',
-    '!source/js/vendor.js'
-  ])
-      .pipe(concat('main.js'))
-      .pipe(gulp.dest('build/js/'));
-});
-
 gulp.task('clean', function () {
   return del('build');
 });
 
-gulp.task('build', gulp.series('clean', 'copy', 'scripts', 'sprite', 'css', 'html'));
+gulp.task('build', gulp.series('clean', 'copy', 'sprite', 'css', 'html'));
 gulp.task('start', gulp.series('build', 'server'));
